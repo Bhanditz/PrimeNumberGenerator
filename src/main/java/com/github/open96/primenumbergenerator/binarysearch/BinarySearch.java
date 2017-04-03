95da1a0db91101058ed04225dec217d4cb928455
Wojciech Hołowiak <w.holowiak@gmail.com>
2017-04-03 10:38:46 +0200

    Added two empty classes that will later be written.

---
+++ src/main/java/com/github/open96/primenumbergenerator/binarysearch/BinarySearch.java
@@ -0,0 +1,7 @@
+package com.github.open96.primenumbergenerator.binarysearch;
+
+/**
+ * Created by end on 03/04/17.
+ */
+public class BinarySearch {
+}
---
+++ src/main/java/com/github/open96/primenumbergenerator/sieve/ErastotenesSieve.java
@@ -0,0 +1,7 @@
+package com.github.open96.primenumbergenerator.sieve;
+
+/**
+ * Created by end on 03/04/17.
+ */
+public class ErastotenesSieve {
+}
