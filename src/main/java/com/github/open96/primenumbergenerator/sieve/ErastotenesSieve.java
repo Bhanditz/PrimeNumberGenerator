74888468d89f0397054a30b1a20e6a8767fe1316
Wojciech Hołowiak <w.holowiak@gmail.com>
2017-04-03 11:28:51 +0200

    Added populateSieve() method.

--- src/main/java/com/github/open96/primenumbergenerator/sieve/ErastotenesSieve.java
+++ src/main/java/com/github/open96/primenumbergenerator/sieve/ErastotenesSieve.java
@@ -1,6 +1,9 @@
 package com.github.open96.primenumbergenerator.sieve;
 
+import java.lang.reflect.Array;
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by end on 03/04/17.
@@ -9,6 +12,7 @@ import java.math.BigInteger;
 public class ErastotenesSieve {
     private final BigInteger limit;
     private final BigInteger sqrt;
+    private List<Boolean> sieve;
 
     private BigInteger squareRootOfBigInteger(BigInteger number){
         BigInteger a = BigInteger.ONE;
@@ -23,10 +27,19 @@ public class ErastotenesSieve {
         }
         return a.subtract(BigInteger.ONE);
     }
-    
+
+    private void populateSieve(){
+        for(BigInteger x= BigInteger.ZERO;x.compareTo(limit)!=0;x=x.add(BigInteger.ONE)){
+            sieve.add(new Boolean(true));
+            System.out.println(x);
+        }
+    }
+
 
     public ErastotenesSieve(BigInteger upperLimit){
         limit=upperLimit;
+        sieve=new ArrayList<Boolean>();
+        populateSieve();
         sqrt=squareRootOfBigInteger(limit);
     }
 }
