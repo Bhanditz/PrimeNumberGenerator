ec264f8d0e338c9055002d3c2bb8f34a4d3b73ae
Wojciech Hołowiak <w.holowiak@gmail.com>
2017-04-03 11:07:18 +0200

    Added method calculating sqrt of BigInterger.

--- src/main/java/com/github/open96/primenumbergenerator/sieve/ErastotenesSieve.java
+++ src/main/java/com/github/open96/primenumbergenerator/sieve/ErastotenesSieve.java
@@ -1,7 +1,32 @@
 package com.github.open96.primenumbergenerator.sieve;
 
+import java.math.BigInteger;
+
 /**
  * Created by end on 03/04/17.
+ * This sieve works on any positive number
  */
 public class ErastotenesSieve {
+    private final BigInteger limit;
+    private final BigInteger sqrt;
+
+    private BigInteger squareRootOfBigInteger(BigInteger number){
+        BigInteger a = BigInteger.ONE;
+        BigInteger b = number.shiftRight(5).add(BigInteger.valueOf(8));
+        while(b.compareTo(a)>=0){
+            BigInteger mid=a.add(b).shiftRight(1);
+            if(mid.multiply(mid).compareTo(number)>0){
+                b=mid.subtract(BigInteger.ONE);
+            } else{
+                a = mid.add(BigInteger.ONE);
+            }
+        }
+        return a.subtract(BigInteger.ONE);
+    }
+    
+
+    public ErastotenesSieve(BigInteger upperLimit){
+        limit=upperLimit;
+        sqrt=squareRootOfBigInteger(limit);
+    }
 }
