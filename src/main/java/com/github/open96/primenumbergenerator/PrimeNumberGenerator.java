9e9ee27f35c7872acb14b3607739df0f7d521e47
Wojciech Hołowiak <w.holowiak@gmail.com>
2017-04-03 10:31:58 +0200

    Initial repository commit.

---
+++ src/main/java/com/github/open96/primenumbergenerator/PrimeNumberGenerator.java
@@ -0,0 +1,13 @@
+package com.github.open96.primenumbergenerator;
+
+/**
+ * Hello world!
+ *
+ */
+public class PrimeNumberGenerator
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
