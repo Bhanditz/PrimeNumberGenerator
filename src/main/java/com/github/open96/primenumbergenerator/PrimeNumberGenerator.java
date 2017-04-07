09b660b4997545893bdf83210a0134251ed7975e
Wojciech Hołowiak <w.holowiak@gmail.com>
2017-04-07 13:09:22 +0200

    Program can now be run with command line arguments.

--- src/main/java/com/github/open96/primenumbergenerator/PrimeNumberGenerator.java
+++ src/main/java/com/github/open96/primenumbergenerator/PrimeNumberGenerator.java
@@ -12,19 +12,24 @@ import com.github.open96.primenumbergenerator.timer.Timer;
  */
 public class PrimeNumberGenerator {
     public static void main(String[] args) {
-        Timer t = new Timer();
-        t.start();
-        int size = 3000000;
-        Sieve sieve;
-        if (size <= 300000) {
-            sieve = new LinearSieve(size);
-        } else {
-            sieve = new EratostenesSieve(size);
+        if(args.length!=1){
+            System.out.println("Please run this program with positive number as an argument");
         }
-        sieve.deleteNonPrimeNumbers();
-        System.out.println(sieve.checkIfNumberIsPrime(9));
-        System.out.println(sieve.countPrimes(0, size));
-        t.stop();
-        t.showTimeInConsole();
+        else {
+            Timer t = new Timer();
+            t.start();
+            int size = Integer.parseInt(args[0]);
+            Sieve sieve;
+            if (size <= 3000000) {
+                sieve = new LinearSieve(size);
+            } else {
+                sieve = new EratostenesSieve(size);
+            }
+            sieve.deleteNonPrimeNumbers();
+            System.out.println(sieve.checkIfNumberIsPrime(9));
+            System.out.println(sieve.countPrimes(0, size));
+            t.stop();
+            t.showTimeInConsole();
+        }
     }
 }
